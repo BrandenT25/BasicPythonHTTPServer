d5e7a965152b27bb2cf4553bbe78a79819e48dc0
Branden <brandent35@tamu.edu>
2025-11-19 18:11:31 -0600

    test for github actions failure

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/bash
 set -e
-venv_path="../venv/bin/"
+venv_path="venv/bin"
 source "$venv_path/activate"
 
 
-cd .. 
+ 
 nohup python BasicPythonHttpServer.py > server.log 2>&1 &
 PID=$!
 sleep 1
 cd tests/
-source ../venv/bin/activate
-pytest test_server.py 
+pytest test_server.py
+sleep 1
 kill $PID
+deactivate
 
 
